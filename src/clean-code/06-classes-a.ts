c163290d5bf7af0a83ac34095b99d7280e7f139f
Kodake <kodake@hotmail.es>
2022-06-23 22:59:21 -0400

    Herencia problemática

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -16,6 +16,49 @@
     const newPerson = new Person('Kodake', 'M', new Date('1991-10-15'));
     console.log({ newPerson });
 
+    class User extends Person {
+        public lastAccess: Date;
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(email, role, name, gender, birthdate)
+        }
+    }
+
+    const userSettings = new UserSettings(
+        '/usr/home',
+        '/home',
+        'kodake@mail.com',
+        'Admin',
+        'Kodake',
+        'M',
+        new Date('1991-10-15')
+    );
+
+    console.log({ userSettings });
 
 })();
 
