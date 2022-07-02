04b5689b5ad5fc854f68069e8eb4fa8359323dc1
Kodake <kodake@hotmail.es>
2022-07-02 12:11:46 -0400

    Code smells & high and low coupling

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -2,30 +2,29 @@
 
     // No aplicando el principio de responsabilidad única
 
-    type Gender = 'M' | 'F';
+    type Gender = 'M'|'F';
 
-    // Forma corta
     class Person {
         constructor(
-            public name: string,
-            public gender: Gender,
+            public name: string, 
+            public gender: Gender, 
             public birthdate: Date
-        ) { }
+        ){}
     }
 
-    const newPerson = new Person('Kodake', 'M', new Date('1991-10-15'));
-    console.log({ newPerson });
 
     class User extends Person {
+        
         public lastAccess: Date;
+
         constructor(
             public email: string,
             public role: string,
             name: string,
             gender: Gender,
-            birthdate: Date
+            birthdate: Date,
         ) {
-            super(name, gender, birthdate);
+            super( name, gender, birthdate );
             this.lastAccess = new Date();
         }
 
@@ -34,43 +33,33 @@
         }
     }
 
+
     class UserSettings extends User {
         constructor(
             public workingDirectory: string,
-            public lastOpenFolder: string,
-            email: string,
-            role: string,
-            name: string,
-            gender: Gender,
-            birthdate: Date
+            public lastOpenFolder  : string,
+            email                  : string,
+            role                   : string,
+            name                   : string,
+            gender                 : Gender,
+            birthdate              : Date
         ) {
-            super(email, role, name, gender, birthdate)
+            super(email, role, name, gender, birthdate );
         }
     }
+
 
     const userSettings = new UserSettings(
         '/usr/home',
         '/home',
-        'kodake@mail.com',
+        'fernando@google.com',
         'Admin',
-        'Kodake',
+        'Fernando',
         'M',
-        new Date('1991-10-15')
+        new Date('1985-10-21')
     );
 
     console.log({ userSettings });
 
+
 })();
-
-// Forma larga
-// class Person {
-//     public name: string;
-//     public gender: Gender;
-//     public birthdate: Date;
-
-//     constructor(name: string, gender: Gender, birthdate: Date) {
-//         this.name = name;
-//         this.gender = gender;
-//         this.birthdate = birthdate;
-//     }
-// }
